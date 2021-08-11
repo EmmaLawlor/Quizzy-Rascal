b8df2a3f6955bd19e75d404d8a860c8937dfb2c8
EmmaLawlor <emmalwlr01@gmail.com>
2021-08-11 21:49:05 +0000

    Add code to keep displaying questions up to a maximum of 10 before ending the quiz

--- assets/js/new.js
+++ assets/js/new.js
@@ -7,7 +7,7 @@ const answerD = document.getElementById('d');
 
 const score = document.getElementById('score-area');
 
-const result = document.getElementsByClassName('result-contrainer');
+const result = document.getElementsByClassName('result-container');
 
 let questionNum;
 let currentQuestion;
@@ -56,243 +56,257 @@ function checkAnswer(userChoice) {
         console.log("Correct!");
         document.getElementById(userChoice).classList.add("correct");
         incrementScore();
-      } else {
+    } else {
         console.log("Incorrect");
-        document.getElementById(userChoice).classList.add("correct");
-      }
+        document.getElementById(userChoice).classList.add("incorrect");
+    }
+
+    currentQuestion++;
+
+    //make sure we have enough questions and don't exceed 10
+    if (questions.length > currentQuestion + 1 && currentQuestion + 1 < 11){
+        setTimeout(function () {
+            displayQuestion();
+        }, 1000);
+    }else{
+        //game is over, show message and button to play
+        document.querySelector('.result-container').classList.remove('hide');
+        
+    }
 }
+
 
 /**
  * Get current score and increments by 1 each time the function is called
  * Code adapted from CI love maths walktrhough project
  */
- function incrementScore() {
+function incrementScore() {
     let currentScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++currentScore;
-  
-  }
+
+}
 
 //List of trivia questions
 let questions = [{
-    question: 'Which rock band was founded by Trent Reznor in 1988?',
-    answers: {
-      a: "Jane's Addiction",
-      b: "Smashing Pumpkins",
-      c: "Nine Inch Nails",
-      d: "Depeche Mode"
+        question: 'Which rock band was founded by Trent Reznor in 1988?',
+        answers: {
+            a: "Jane's Addiction",
+            b: "Smashing Pumpkins",
+            c: "Nine Inch Nails",
+            d: "Depeche Mode"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "In which year did Elvis Presley die?",
-    answers: {
-      a: "1976",
-      b: "1977",
-      c: "1978",
-      d: "1979"
+    {
+        question: "In which year did Elvis Presley die?",
+        answers: {
+            a: "1976",
+            b: "1977",
+            c: "1978",
+            d: "1979"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "Jimmy, Robert, John and John: can you identify this rock band from the first names of the oridinal line-up?",
-    answers: {
-      a: "Led Zeppelin",
-      b: "AC/DC",
-      c: "The Who",
-      d: "Pink Floyd"
+    {
+        question: "Jimmy, Robert, John and John: can you identify this rock band from the first names of the oridinal line-up?",
+        answers: {
+            a: "Led Zeppelin",
+            b: "AC/DC",
+            c: "The Who",
+            d: "Pink Floyd"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "Reginald Kenneth Dwight is better known by what name?",
-    answers: {
-      a: "Prince",
-      b: "Elton John",
-      c: "Meatloaf",
-      d: "Sting"
+    {
+        question: "Reginald Kenneth Dwight is better known by what name?",
+        answers: {
+            a: "Prince",
+            b: "Elton John",
+            c: "Meatloaf",
+            d: "Sting"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "'Ten' was the 1991 debut album of which band?",
-    answers: {
-      a: "Nirvana",
-      b: "Soundgarden",
-      c: "Alice in Chains",
-      d: "Pearl Jam"
+    {
+        question: "'Ten' was the 1991 debut album of which band?",
+        answers: {
+            a: "Nirvana",
+            b: "Soundgarden",
+            c: "Alice in Chains",
+            d: "Pearl Jam"
+        },
+        correctAnswer: "d"
     },
-    correctAnswer: "d"
-  },
-  {
-    question: "Which Bob Dylan song did Adele cover on her first album?",
-    answers: {
-      a: "Blowin' in the Wind",
-      b: "Like a Rolling Stone",
-      c: "To Make You Feel My Love",
-      d: "Masters of War"
+    {
+        question: "Which Bob Dylan song did Adele cover on her first album?",
+        answers: {
+            a: "Blowin' in the Wind",
+            b: "Like a Rolling Stone",
+            c: "To Make You Feel My Love",
+            d: "Masters of War"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "Roger Taylor is the drummer in which band?",
-    answers: {
-      a: "Queen",
-      b: "Kiss",
-      c: "Aerosmith",
-      d: "The Rolling Stones"
+    {
+        question: "Roger Taylor is the drummer in which band?",
+        answers: {
+            a: "Queen",
+            b: "Kiss",
+            c: "Aerosmith",
+            d: "The Rolling Stones"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "Eminem's '8 Mile' is named after a road in which city?",
-    answers: {
-      a: "Chicago",
-      b: "Seattle",
-      c: "Michigan",
-      d: "Detroit"
+    {
+        question: "Eminem's '8 Mile' is named after a road in which city?",
+        answers: {
+            a: "Chicago",
+            b: "Seattle",
+            c: "Michigan",
+            d: "Detroit"
+        },
+        correctAnswer: "d"
     },
-    correctAnswer: "d"
-  },
-  {
-    question: "Who was the first woman to be inducted into the Rock and Roll Hall of Fame?",
-    answers: {
-      a: "LaVern Baker",
-      b: "Tina Turner",
-      c: "Aretha Franklin",
-      d: "Stevie Nicks"
+    {
+        question: "Who was the first woman to be inducted into the Rock and Roll Hall of Fame?",
+        answers: {
+            a: "LaVern Baker",
+            b: "Tina Turner",
+            c: "Aretha Franklin",
+            d: "Stevie Nicks"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "What was Jimi Hendrix's only Billboard Top 40 hit?",
-    answers: {
-      a: "Bold As Love",
-      b: "All Along the Watchtower",
-      c: "Purple Haze",
-      d: "Voodoo Child"
+    {
+        question: "What was Jimi Hendrix's only Billboard Top 40 hit?",
+        answers: {
+            a: "Bold As Love",
+            b: "All Along the Watchtower",
+            c: "Purple Haze",
+            d: "Voodoo Child"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "Van Halen famously banned which color M&M's from their rider?",
-    answers: {
-      a: "Brown",
-      b: "Yellow",
-      c: "Blue",
-      d: "Red"
+    {
+        question: "Van Halen famously banned which color M&M's from their rider?",
+        answers: {
+            a: "Brown",
+            b: "Yellow",
+            c: "Blue",
+            d: "Red"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "Though made famous by Ike & Tina Turner, 'Proud Mary' was first recorded by whom?",
-    answers: {
-      a: "The Doors",
-      b: "Lynyrd Synyrd",
-      c: "Led Zeppelin",
-      d: "Creedence Clearwater Revival"
+    {
+        question: "Though made famous by Ike & Tina Turner, 'Proud Mary' was first recorded by whom?",
+        answers: {
+            a: "The Doors",
+            b: "Lynyrd Synyrd",
+            c: "Led Zeppelin",
+            d: "Creedence Clearwater Revival"
+        },
+        correctAnswer: "d"
     },
-    correctAnswer: "d"
-  },
-  {
-    question: "Actor Jared Leto is the frontman of which band?",
-    answers: {
-      a: "Linkin Park",
-      b: "Green Day",
-      c: "30 Seconds to Mars",
-      d: "The Used"
+    {
+        question: "Actor Jared Leto is the frontman of which band?",
+        answers: {
+            a: "Linkin Park",
+            b: "Green Day",
+            c: "30 Seconds to Mars",
+            d: "The Used"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "Which member of Kiss wears makeup to look like a cat?",
-    answers: {
-      a: "Vinnie Vincent",
-      b: "Peter Criss",
-      c: "Paul Stanley",
-      d: "Gene Simmons"
+    {
+        question: "Which member of Kiss wears makeup to look like a cat?",
+        answers: {
+            a: "Vinnie Vincent",
+            b: "Peter Criss",
+            c: "Paul Stanley",
+            d: "Gene Simmons"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "Which rockstar's memoir is titled 'The Heroin Diaries'?",
-    answers: {
-      a: "Nikki Sixx",
-      b: "Ozzy Osbourne",
-      c: "Anthony Keidis",
-      d: "Neil Young"
+    {
+        question: "Which rockstar's memoir is titled 'The Heroin Diaries'?",
+        answers: {
+            a: "Nikki Sixx",
+            b: "Ozzy Osbourne",
+            c: "Anthony Keidis",
+            d: "Neil Young"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "The 'J' in Mary J Blige stands for what?",
-    answers: {
-      a: "June",
-      b: "Jane",
-      c: "Joan",
-      d: "Jean"
+    {
+        question: "The 'J' in Mary J Blige stands for what?",
+        answers: {
+            a: "June",
+            b: "Jane",
+            c: "Joan",
+            d: "Jean"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "Lizzo plays which instrument?",
-    answers: {
-      a: "Saxaphone",
-      b: "Violin",
-      c: "Flute",
-      d: "Piana"
+    {
+        question: "Lizzo plays which instrument?",
+        answers: {
+            a: "Saxaphone",
+            b: "Violin",
+            c: "Flute",
+            d: "Piana"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "Which group recorded the hit 'Karma Chameleon' in 1984?",
-    answers: {
-      a: "Culture Club",
-      b: "Duran Duran",
-      c: "Spandau Ballet",
-      d: "The Pet Shop Boys"
+    {
+        question: "Which group recorded the hit 'Karma Chameleon' in 1984?",
+        answers: {
+            a: "Culture Club",
+            b: "Duran Duran",
+            c: "Spandau Ballet",
+            d: "The Pet Shop Boys"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "What was the name of the Spice Girls' 1996 debut album?",
-    answers: {
-      a: "Spiceworld",
-      b: "Forever",
-      c: "Wannabe",
-      d: "Spice"
+    {
+        question: "What was the name of the Spice Girls' 1996 debut album?",
+        answers: {
+            a: "Spiceworld",
+            b: "Forever",
+            c: "Wannabe",
+            d: "Spice"
+        },
+        correctAnswer: "d"
     },
-    correctAnswer: "d"
-  },
-  {
-    question: "Who is the lead singer of Smash Mouth?",
-    answers: {
-      a: "Sean Hurwitz",
-      b: "Randy Cooke",
-      c: "Steve Harwell",
-      d: "Paul de Lisle"
+    {
+        question: "Who is the lead singer of Smash Mouth?",
+        answers: {
+            a: "Sean Hurwitz",
+            b: "Randy Cooke",
+            c: "Steve Harwell",
+            d: "Paul de Lisle"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "Which Pink Floyd album features a prism on the cover?",
-    answers: {
-      a: "Dark side of the moon",
-      b: "Animals",
-      c: "The Wall",
-      d: "The Division Bell"
+    {
+        question: "Which Pink Floyd album features a prism on the cover?",
+        answers: {
+            a: "Dark side of the moon",
+            b: "Animals",
+            c: "The Wall",
+            d: "The Division Bell"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "What does the Italian musical term 'adagio' mean?",
-    answers: {
-      a: "Loud",
-      b: "Quiet",
-      c: "Fast",
-      d: "Slow"
-    },
-    correctAnswer: "d"
-  }
+    {
+        question: "What does the Italian musical term 'adagio' mean?",
+        answers: {
+            a: "Loud",
+            b: "Quiet",
+            c: "Fast",
+            d: "Slow"
+        },
+        correctAnswer: "d"
+    }
 ];
 
 playQuiz();
